7fac11d4b6db356850680b5aa79b9332a5928e79
fynn <fynn091209@proton.me>
2024-08-17 16:06:53 +0200

    first doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+// use std::error::Error;
+
 #[derive(Debug)]
 pub enum Priority {
     A,
@@ -16,4 +18,27 @@ pub struct Task {
     pub task: String,
     pub priority: Priority,
     pub status: Status,
+}
+
+impl Task {
+    /// Builds todo struct
+    pub fn build(args: &[String]) -> Result<Task, &'static str> {
+        let mut todo_instance = Task {
+            task: String::from("toBeFilledLater"),
+            status: Status::Open,
+            priority: Priority::B,
+        };
+        if args.len() == 1 {
+            //TODO: print out all todos or open tui to show and navigate all todos
+            return Err("too little for now");
+        } else if args.len() == 2 {
+            todo_instance = Task {
+                task: args[1].clone(),
+                ..todo_instance
+            };
+            return Ok(todo_instance);
+        } else {
+            return Err("too much for now");
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
-use std::cmp::Ordering;
 use std::env;
+use std::process;
 use td;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // with any input the len is one
-    let default_len = 2;
-    match &args.len().cmp(&default_len) {
-        Ordering::Less => {
-            //TODO: print out list of todos
-        }
-        Ordering::Equal => {
-            // let todo_instance = td::Status::Open(td::PrioOfTask::B(args[1].clone()));
-            let todo_instance = td::Task {
-                task: args[1].clone(),
-                status: td::Status::Open,
-                priority: td::Priority::B,
-            };
-            println!("{:?}", todo_instance);
-        }
-        Ordering::Greater => {
-            //TODO: either usage guide or selecting prio
-        }
+    let todo_path: &str = "/home/fynn/todo.txt";
+    if args.len() == 1 {
+        //TODO: print out the file
+        panic!("for now -> at least one thing has to be appended to the call");
     }
+    let todo_instance = td::Task::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    //match &args.len().cmp(&default_len) {
+    //    Ordering::Less => {
+    //        //TODO: print out list of todos
+    //    }
+    //    Ordering::Equal => {
+    //        // let todo_instance = td::Status::Open(td::PrioOfTask::B(args[1].clone()));
+    //        let todo_instance = td::Task {
+    //            task: args[1].clone(),
+    //            status: td::Status::Open,
+    //            priority: td::Priority::B,
+    //        };
+    //        println!("{:?}", todo_instance);
+    //    }
+    //    Ordering::Greater => {
+    //        //TODO: either usage guide or selecting prio
+    //    }
+    //}
 }
